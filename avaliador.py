027ac42a198ed3867d9d990dc9f92e9107821709
Fabricio Magalhaes <fabricio.mrj@hotmail.com>
2023-05-28 22:09:54 -0300

    corrigindo

--- avaliador.py
+++ avaliador.py
@@ -1,6 +1,7 @@
 import csv
 from collections import defaultdict
 import pandas as pd
+import numpy as np
 import math
 import matplotlib.pyplot as plt
 
@@ -71,7 +72,7 @@ def avaliador(result_arquive):
 
             y_pred.append(1)
 
-            if num_recover == num_relevances:
+            if num_recover == 10:
                 # Calcular F1 Score para essa consulta
                 f1 = f1_score(y_true, y_pred)
                 f1_scores.append(f1) 
@@ -89,7 +90,7 @@ def avaliador(result_arquive):
                 query = int(query)
                 precision_table.loc[query]['precision@10'] = precision/100
             
-            if num_recover == num_relevances:
+            if num_recover == 10:
                 R_precision.append(num_recover_relevance/num_relevances)
 
             if num_recover <= num_relevances:
@@ -123,14 +124,14 @@ def avaliador(result_arquive):
 
     tabela_map_mrr = pd.DataFrame(data=[[mean_of_mean_precision,media_rr]], index=["valores"] ,columns=["map","mrr"])
 
-    return lista_x, eleven_recall_total, R_precision, precision_table,f1_scores
+    return lista_x, eleven_recall_total, R_precision, precision_table,f1_scores,tabela_map_mrr
 
 
 if __name__ == "__main__":
     steam='results-stemmer.csv'
     nosteam='results-nostemmer.csv'
-    x1,y1,R_precision1 ,precision_table1,f1_scores1= avaliador(steam)
-    x2,y2,R_precision2 ,precision_table2,f1_scores2= avaliador(nosteam)
+    x1,y1,R_precision1 ,precision_table1,f1_scores1,tabela_map_mrr1= avaliador(steam)
+    x2,y2,R_precision2 ,precision_table2,f1_scores2,tabela_map_mrr2= avaliador(nosteam)
 
 
     plt.plot( x1,y1, color='blue', label='Stemmer',linestyle='solid', linewidth = 3, marker='o', markerfacecolor='black', markersize=6)
@@ -147,21 +148,127 @@ if __name__ == "__main__":
 
 
 
-    plt.hist(R_precision1,  alpha=0.5, label='Stemmer')
-    plt.hist(R_precision2, alpha=0.5, label='NoStemmer')
-    plt.xlabel('Precision')
-    plt.ylabel('Frequency')
-    plt.title('Histograma Comparativo')
-    plt.legend()
+    r_a_b=np.array(R_precision1)-np.array(R_precision2)
+    plt.figure(figsize=(30,10))
+    # Definir as cores para valores positivos e negativos
+    collors = ['orange' if valor < 0 else 'blue' for valor in r_a_b]
 
+    # Plotar o gráfico de barras com cores personalizadas
+    plt.bar(range(1,len(r_a_b)+1), r_a_b, color=collors)
+
+    # # Adicionar legenda
+    legenda_cores = {'Steam': 'blue', 'NoSteam': 'orange'}
+    patches = [plt.Rectangle((0, 0), 1, 1, color=color) for color in legenda_cores.values()]
+    plt.legend(patches, legenda_cores.keys(), loc='upper right')
+
+    # Adicionar rótulos e título
+    plt.ylabel('R-Precision A/B')
+    plt.title('R-Precision A/B por Query Number')
+    plt.xticks(range(1,len(r_a_b)+1))
+    plt.gca().yaxis.grid()
+    
     plt.savefig("avalia/histograma.pdf" )
 
 
+
+
+
     plt.figure(figsize=(10,5))
-    plt.plot(range(1, len(f1_scores1) + 1), f1_scores1, marker='o', linestyle='-',label='Stemmer')
-    plt.plot(range(1, len(f1_scores2) + 1), f1_scores2, marker='o', linestyle='-',label='NoStemmer')
-    plt.title('F1 Score for each Query')
-    plt.xlabel('Query Number')
-    plt.ylabel('F1 Score')
-    plt.legend()
-    plt.savefig("avalia/f1_score.pdf" )
+    # Calcular as médias das colunas
+    media_f1_s = np.mean(f1_scores1)
+    meadia_f1_ns =np.mean( f1_scores2)
+
+    # Plotar o gráfico de barras com as médias
+    categorias = ['Steammer', 'NoSteammer']
+    medias = [media_f1_s, meadia_f1_ns]
+
+    plt.bar(categorias, medias,color = ['blue', 'orange'])
+    for i, media in enumerate(medias):
+        plt.text(i, media, f'{media:.2%}', ha='center', va='bottom')
+
+    # Adicionar rótulos e título
+    plt.ylabel('f1_score médio (%)')
+    plt.title('f1_score')
+
+
+    plt.savefig("avalia/f1_score@10.pdf" )
+
+
+
+
+
+    # Calcular as médias das colunas
+    media_precision_5_s = precision_table1['precision@5'].mean()
+    media_precision_5_ns = precision_table2['precision@5'].mean()
+
+    # Plotar o gráfico de barras com as médias
+    categorias = ['Steammer', 'NoSteammer']
+    medias = [media_precision_5_s, media_precision_5_ns]
+    plt.figure(figsize=(10,5))
+    plt.bar(categorias, medias,color = ['blue', 'orange'])
+    for i, media in enumerate(medias):
+        plt.text(i, media, f'{media:.2%}', ha='center', va='bottom')
+
+    # Adicionar rótulos e título
+    plt.ylabel('P@5 médio (%)')
+    plt.title('Precision@5')
+    plt.savefig("avalia/precision@5.pdf" )
+
+
+
+
+
+    # Calcular as médias das colunas
+    media_precision_10_s = precision_table1['precision@10'].mean()
+    media_precision_10_ns = precision_table2['precision@10'].mean()
+
+    # Plotar o gráfico de barras com as médias
+    categorias = ['Steammer', 'NoSteammer']
+    medias = [media_precision_10_s, media_precision_10_ns]
+    plt.figure(figsize=(10,5))
+    plt.bar(categorias, medias,color = ['blue', 'orange'])
+    for i, media in enumerate(medias):
+        plt.text(i, media, f'{media:.2%}', ha='center', va='bottom')
+
+    # Adicionar rótulos e título
+    plt.ylabel('P@10 médio (%)')
+    plt.title('Precision@10')
+    plt.savefig("avalia/precision@10.pdf" )
+    
+
+
+    # Calcular as médias das colunas
+    media_map1= tabela_map_mrr1['map'].mean()
+    media_map2 = tabela_map_mrr2['map'].mean()
+
+    # Plotar o gráfico de barras com as médias
+    categorias = ['Steammer', 'NoSteammer']
+    medias = [media_map1, media_map2]
+    plt.figure(figsize=(10,5))
+    plt.bar(categorias, medias,color = ['blue', 'orange'])
+    for i, media in enumerate(medias):
+        plt.text(i, media, f'{media:.2%}', ha='center', va='bottom')
+
+    # Adicionar rótulos e título
+    plt.ylabel('map médio (%)')
+    plt.title('map')
+    plt.savefig("avalia/map.pdf" )
+
+
+    # Calcular as médias das colunas
+    media_mrr1= tabela_map_mrr1['mrr'].mean()
+    media_mrr2 = tabela_map_mrr2['mrr'].mean()
+
+    # Plotar o gráfico de barras com as médias
+    categorias = ['Steammer', 'NoSteammer']
+    medias = [media_mrr1, media_mrr2]
+    plt.figure(figsize=(10,5))
+    plt.bar(categorias, medias,color = ['blue', 'orange'])
+    for i, media in enumerate(medias):
+        plt.text(i, media, f'{media:.2%}', ha='center', va='bottom')
+
+    # Adicionar rótulos e título
+    plt.ylabel('mrr médio (%)')
+    plt.title('mrr')
+
+    plt.savefig("avalia/mrr.pdf" )
